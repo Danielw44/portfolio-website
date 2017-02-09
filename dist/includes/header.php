15d961543e2274e002eebd41fcced6fd028cab24
Danielw44 <daniel.a.worthington44@gmail.com>
2017-02-09 14:22:31 +0000

    Setup Files

---
+++ dist/includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Daniel Worthington</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+  <nav class="site-nav">
+    <div class="site-container nav-container">
+      <div class="logo"><a href="index.php">Daniel Worthington</a></div>
+
+      <div class="mobile-nav">
+        <div class="inner">
+          <span class="icon-bar top"></span>
+          <span class="icon-bar middle"></span>
+          <span class="icon-bar bottom"></span>
+        </div>
+      </div>
+
+      <div class="site-nav__items" style="display:none">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Work</a>
+        <a href="#">Experience</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  </nav>
---
+++ dist/index.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php' ?>
+
+  <div class="site-container">
+    <section class="intro">
+      <div class="intro-text">
+        <h1>Welcome</h1>
+        <h2>I am... a Web Developer</h2>
+      </div>
+    </section>
+  </div>
+
+<?php include 'includes/footer.php' ?>
